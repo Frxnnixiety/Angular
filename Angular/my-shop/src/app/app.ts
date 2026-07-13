cd7cb0c0fadce589b4463d16dc9fc2bcf8e0c55b
Frxnnixiety <franceliaelguera@gmail.com>
2026-07-12 20:23:45 -0600

    Angular API

--- Angular/my-shop/src/app/app.ts
+++ Angular/my-shop/src/app/app.ts
@@ -1,12 +1,52 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+// Importamos el servicio y la interfaz estructurada hace un momento
+import { Api, Products} from './services/api';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  templateUrl: './app.html', // Vinculado a tu archivo HTML
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('my-shop');
+export class App implements OnInit {
+  
+  // Este método se ejecuta automáticamente cuando se genera la vista en el navegador
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  protected readonly title = signal('eshop');
+  
+  // Inyección del servicio de datos en el componente
+  private readonly apiService = inject(Api);  
+  
+  // Se crea una señal (signal) para detectar de forma reactiva los cambios del arreglo
+  productList = signal<Products[]>([]);
+
+  /**
+   * Método para obtener los productos en formato JSON
+   */
+  getProducts() {
+    this.apiService.getProducts().subscribe({
+      next: (data) => {
+        this.productList.set(data); // Actualizamos el estado de la señal con los datos recibidos
+        console.log(data);
+      },
+      error: (err) => console.error('Error en GET:', err)
+    });
+  }
+
+  /**
+   * Método para eliminar un producto seleccionando su ID
+   */
+  deleteProduct(id: number) {
+    this.apiService.deleteProduct(id).subscribe({
+      next: (data) => {
+        this.getProducts(); // Refrescamos la lista actual instalada
+      },
+      error: (err) => console.error('Error en DELETE:', err)
+    });
+  }
 }
--- Angular/my-shop/src/app/services/api.ts
+++ Angular/my-shop/src/app/services/api.ts
@@ -1,4 +1,56 @@
-import { Service } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Service()
-export class Api {}
+// Interfaz para definir la estructura de los datos del producto
+export interface Products {
+  id: number;
+  nombre: string;
+  precio: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api {
+  // Cliente http para realizar las consultas externas
+  private http = inject(HttpClient);
+  
+  // URL de la API proporcionada por el instituto
+  private apiUrl = 'https://fakestoreapi.com/products';
+
+  /**
+   * MÉTODO GET: Obtener datos de la API
+   */
+  getProducts(): Observable<Products[]> {
+    return this.http.get<Products[]>(this.apiUrl);
+  }
+
+  /**
+   * MÉTODO POST: Crear un nuevo producto
+   */
+  createProduct(product: Products): Observable<Products> {
+    return this.http.post<Products>(this.apiUrl, product);
+  }
+
+  /**
+   * MÉTODO PUT: Reemplazar un recurso existente por completo
+   */
+  updateProductComplete(id: number, producto: Products): Observable<Products> {
+    return this.http.put<Products>(`${this.apiUrl}/${id}`, producto);
+  }
+
+  /**
+   * MÉTODO PATCH: Actualizar parcialmente un recurso
+   */
+  updatePproductPartial(id: number, cambios: Partial<Products>): Observable<Products> {
+    return this.http.patch<Products>(`${this.apiUrl}/${id}`, cambios);
+  }
+
+  /**
+   * MÉTODO DELETE: Eliminar un recurso
+   */
+  deleteProduct(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
